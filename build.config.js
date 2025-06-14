605a64229ded14b618ac089981b5f10f236fa018
Tal Kohavy <talkohavy@gmail.com>
2025-06-14 10:00:07 +0300

    replaced the build process

--- build.config.js
+++ build.config.js
@@ -1,69 +1,99 @@
 import { execSync } from 'child_process';
 import fs, { cpSync } from 'fs';
+import path from 'path';
+
+/**
+ * @typedef {{
+ *   version: string,
+ *   private?: string | boolean,
+ *   main: string,
+ *   type: 'module' | 'commonjs'
+ *   types: string,
+ *   scripts?: Record<string, string>,
+ *   publishConfig: {
+ *     access: string
+ *   },
+ *   devDependencies?: Record<string, string>,
+ * }} PackageJson
+ */
 
 const outDirName = 'dist';
+
+const ROOT_PROJECT = process.cwd();
+const greenColor = '[32m';
+const blueColor = '[34m';
+const stopColor = '[39m';
 
 buildPackageConfig();
 
 async function buildPackageConfig() {
   cleanDistDirectory();
 
-  buildWithTsc();
+  build();
 
-  copyReadmeFile();
+  copyStaticFiles();
 
-  copyAndManipulatePackageJsonFile();
-
-  copyChangesetDirectory();
-
-  copyNpmIgnore();
+  manipulatePackageJsonFile();
 
   console.log('DONE !!!');
 }
 
 function cleanDistDirectory() {
-  console.log('- Step 1: clear the dist directory');
-  execSync('rm -rf dist');
+  console.log(`${greenColor}- Step 1:${stopColor} clear the ${outDirName} directory`);
+  execSync(`rm -rf ${outDirName}`);
 }
 
-function buildWithTsc() {
-  console.log('- Step 2: build with tsc');
-  execSync('tsc -p tsconfig.json');
+function build() {
+  console.log(`${greenColor}- Step 2:${stopColor} build`);
+  execSync('rollup --config'); // or the full command: rollup --config rollup.config.mjs
 }
 
-function copyReadmeFile() {
-  console.log('- Step 3: copy the README.md file');
-  const readStreamReadmeMd = fs.createReadStream('./README.md');
-  const writeStreamReadmeMd = fs.createWriteStream(`./${outDirName}/README.md`);
-  readStreamReadmeMd.pipe(writeStreamReadmeMd);
+function copyStaticFiles() {
+  console.log(`${greenColor}- Step 3:${stopColor} copy static files`);
+
+  const filesToCopyArr = [
+    { filename: 'package.json', sourceDirPath: [], destinationDirPath: [] },
+    { filename: '.npmignore', sourceDirPath: [], destinationDirPath: [] },
+    { filename: '.npmrc', sourceDirPath: [], destinationDirPath: [], isAllowedToFail: true },
+    { filename: 'README.md', sourceDirPath: [], destinationDirPath: [] },
+  ];
+
+  filesToCopyArr.forEach(({ filename, sourceDirPath, destinationDirPath, isAllowedToFail }) => {
+    try {
+      const sourceFileFullPath = path.resolve(ROOT_PROJECT, ...sourceDirPath, filename);
+      const destinationFileFullPath = path.resolve(ROOT_PROJECT, outDirName, ...destinationDirPath, filename);
+
+      cpSync(sourceFileFullPath, destinationFileFullPath);
+      console.log(`    • ${filename}`);
+    } catch (error) {
+      console.error(error);
+      if (isAllowedToFail) return;
+
+      throw new Error('File MUST exists in order to PASS build process! cp operation failed...');
+    }
+  });
 }
 
-function copyAndManipulatePackageJsonFile() {
-  console.log('- Step 4: copy & manipulate the package.json file');
-  // Step 1: get the original package.json file
-  const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+function manipulatePackageJsonFile() {
+  console.log(`${greenColor}- Step 4:${stopColor} copy & manipulate the package.json file`);
 
-  // Step 2: Remove all scripts
-  delete packageJson.scripts;
-  console.log('-- deleted `scripts` key');
+  const packageJsonPath = path.resolve(ROOT_PROJECT, outDirName, 'package.json');
 
-  // Step 3: Change from private to public
+  // Step: get the original package.json file
+  /** @type {PackageJson} */
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+
+  packageJson.type = 'commonjs';
   delete packageJson.private;
+  delete packageJson.scripts;
+  delete packageJson.devDependencies;
   packageJson.publishConfig.access = 'public';
-  console.log('-- changed from private to public');
-  console.log('-- changed publishConfig access to public');
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
 
-  // Step 4: create new package.json file in the output folder
-  fs.writeFileSync(`./${outDirName}/package.json`, JSON.stringify(packageJson));
-  console.log('-- package.json file written successfully!');
-}
-
-function copyChangesetDirectory() {
-  console.log('- Step 5: copy the .changeset directory');
-  cpSync('.changeset', `${outDirName}/.changeset`, { recursive: true });
-}
-
-function copyNpmIgnore() {
-  console.log('- Step 6: copy the .npmignore file');
-  cpSync('.npmignore', `${outDirName}/.npmignore`);
+  console.log(`  • ${blueColor}changed${stopColor} type module to commonjs`);
+  console.log(`  • ${blueColor}changed${stopColor} from private to public`);
+  console.log(`  • ${blueColor}deleted${stopColor} "scripts" key`);
+  console.log(`  • ${blueColor}deleted${stopColor} "devDependencies" key`);
+  console.log(`  • ${blueColor}changed${stopColor} publishConfig access to public`);
+  console.log(`  • ${blueColor}package.json${stopColor} file written successfully!`);
 }
